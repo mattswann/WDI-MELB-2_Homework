8d79b97572352904a49378996881940cb1986f61
Beta Ngadiman <ThirtySevenCelsiusAir@Betas-MacBook-Air.local>
2015-03-12 05:00:10 +1100

    javaScript Homework 2

---
+++ Beta Ngadiman/01_javaScript/javascript.js
@@ -0,0 +1,17 @@
+function doSomething() {
+	var userAge = document.getElementById("userAge").value;
+	var title = document.getElementById("title");
+	
+	if(userAge >= 18){
+		title.innerHTML="Drink like no tomorrow";
+		document.body.style.background = "white";
+		document.body.style.color = "black";
+	} else {
+		title.innerHTML="Come back when you are above 17";
+		document.body.style.background = "red";
+		document.body.style.color = "white";
+	}
+}
+
+var btnEnter = document.getElementById("btnEnter");
+btnEnter.addEventListener('click', doSomething);
