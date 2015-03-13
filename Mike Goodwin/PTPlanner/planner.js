2572c5964669ec04b9b5e4f0429380b715d1422c
Mike Goodwin <goodo73@gmail.com>
2015-03-13 22:47:30 +1100

    Included bonus tasks and re-added initial js under new name

--- Mike Goodwin/PTPlanner/planner.js
+++ Mike Goodwin/PTPlanner/planner.js
@@ -1,59 +1,113 @@
 var metro = {
 	Alamein: ["Flinders Street", "Richmond", "East Richmond", "Burnley", "Hawthorn", "Glenferrie"],
-	"Glen Waverly": ["Flagstaff", "Melbourne Central", "Parliament", "Richmond", "Kooyong", "Tooronga"],
-	Sandringham: ["Southern Cross", "Richmond", "South Yarra", "Prahran", "Windsor"]
+	"Glen Waverly": ["Flagstaff", "Melbourne Central", "Richmond", "Kooyong", "Tooronga"],
+	Sandringham: ["Southern Cross", "Richmond", "South Yarra", "Prahran", "Windsor"],
+	Pakenham: ["Parliament","Richmond","Hawksburn","Toorak","Armadale","Malvern","Caulfield"]
+}
+// Parliament removed from the Glen Waverly line to create the Pakenham line
+
+var lines = Object.keys(metro);
+// var userLineStart = "Alamein";
+// var userStopStart = "Glenferrie";
+// var userLineEnd = "Alamein";
+// var userStopEnd = "Hawthorn";
+
+var userLineStart = prompt("On which line do you wish to start your journey?");
+while (!validEntry(lines,userLineStart)) {
+	var userLineStart = prompt("That line is not valid. Please re-enter.");
+}
+
+var userStopStart = prompt("At which stop do you wish to start your journey?");
+while (!validEntry(metro[userLineStart],userStopStart)) {
+	var userStopStart = prompt("That line is not valid. Please re-enter.");
+}
+
+var userLineEnd = prompt("On which line do you wish to end your journey?");
+while (!validEntry(lines,userLineEnd)) {
+	var userLineEnd = prompt("That line is not valid. Please re-enter.");
+}
+
+var userStopEnd = prompt("At which stop do you wish to end your journey?");
+while (!validEntry(metro[userLineEnd],userStopEnd)) {
+	var userStopEnd = prompt("That line is not valid. Please re-enter.");
 }
 
 var interchange = "Richmond";
-var userLineFirst = "Alamein";
-var userStopFirst = "Glenferrie";
-var userLineLast = "Sandringham";
-var userStopLast = "Windsor";
 var totalStops = 0;
-var indexStart;
-var indexFinish;
-var currLine = metro[userLineFirst];
+var journeyLeg;
+var msg = "You're journeying from " + userStopStart;
+msg += ", on the " + userLineStart + " line, to ";
+msg += userStopEnd + ", on the " + userLineEnd + " line.";
 
-console.log(userLineFirst + ": " + userStopFirst + " to " + userLineLast + ": " + userStopLast);
+console.log(msg);
 
-if (userLineFirst === userLineLast) {
+if (userLineStart === userLineEnd) {
+// Start and end stations are on the same line
 
-	indexStart = findIndex(metro[userLineFirst],userStopFirst);
-	indexFinish = findIndex(metro[userLineFirst],userStopLast);
+	// Calculate the trip between the start station and the end station on the line
+	journeyLeg = getStops(metro[userLineStart],userStopStart,userStopEnd)
+	totalStops += journeyLeg.length - 1;
 
-	totalStops += countStops(indexStart,indexFinish);
-
+	displayStops(journeyLeg);
 } else {
+// Start and end stations are on different lines
 
-	indexStart = findIndex(metro[userLineFirst],userStopFirst);
-	indexFinish = findInterchange(userLineFirst);
+	// Calculate the trip between the start station and the interchange station on the first line
+	journeyLeg = getStops(metro[userLineStart],userStopStart,interchange)
+	totalStops += journeyLeg.length - 1;
 
-	totalStops += countStops(indexStart,indexFinish);
+	displayStops(journeyLeg);
 
-	indexStart = findInterchange(userLineLast);
-	indexFinish = findIndex(metro[userLineLast],userStopLast);
+	// Calculate the trip between the interchange station and the end station on the 2nd line
+	journeyLeg = getStops(metro[userLineEnd],interchange,userStopEnd)
+	totalStops += journeyLeg.length - 1;
+	
+	// Prevent the interchange station from being displayed twice
+	journeyLeg.shift();
 
-	totalStops += countStops(indexStart,indexFinish);
+	displayStops(journeyLeg);
 }
 
-console.log("totalStops : " + totalStops);
+console.log("Your journey will take you through " + totalStops + " stops.");
 
-function countStops (start,stop) {
-// Computes the difference between two values and returns it
-	var stops;
+/*------------------------------------------*/
+/*          F U N C T I O N S               */
+/*------------------------------------------*/
 
-	if (start < stop) {
-		stops = stop - start;
+function validEntry (array,value) {
+// Tests if a specified item exists with the provided array
+	if (array.indexOf(value) === -1) {
+		return false;
 	} else {
-		stops = start - stop;
+		return true;
+	}
+}
+
+function displayStops (journey) {
+// Display the stops on the journey array
+	for (var i = 0; i < journey.length; i++) {
+		console.log(journey[i]);
+	}
+}
+
+function getStops (line,start,end) {
+// Return array of stops between two stops (inclusive) on the provided line
+	var ixStart = findIndex(line,start);
+	var ixEnd = findIndex(line,end);
+
+	var leg = [];
+
+	if (ixStart < ixEnd) {
+		for (var i = ixStart; i <= ixEnd; i++) {
+			leg.push(line[i]);
+		}
+	} else {
+		for (var i = ixStart; i >= ixEnd; i--) {
+			leg.push(line[i]);
+		}
 	}
 
-	return stops;
-}
-
-function findInterchange (line) {
-// Determines the index value of the nominated interchange station
-	return metro[line].indexOf(interchange);
+	return leg;
 }
 
 function findIndex (line,stop) {
