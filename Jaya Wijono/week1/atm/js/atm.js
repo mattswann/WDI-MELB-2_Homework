6f02642d7a1fb05551fc8d5eace722d18846438d
jayzz55 <jayzzwijono@yahoo.com>
2015-03-14 22:00:55 +1100

    complete atm homework

--- Jaya Wijono/week1/atm/js/atm.js
+++ Jaya Wijono/week1/atm/js/atm.js
@@ -6,16 +6,19 @@ var checkingWithdraw = document.querySelector("#checkingWithdraw");
 var savingsAmount = document.querySelector("#savingsAmount");
 var savingsDeposit = document.querySelector("#savingsDeposit");
 var savingsWithdraw = document.querySelector("#savingsWithdraw");
+var excludeDollarSign = 1;
 
 checkCheckingBalanceZero();
 checkSavingsBalanceZero();
 
-checkingDeposit.addEventListener('click',checkCheckingBalanceZero);
-checkingWithdraw.addEventListener('click',doSomething);
-savingsDeposit.addEventListener('click',deposit(checkingAccount.innerHTML,'savings'));
-savingsWithdraw.addEventListener('click',doSomething);
+checkingDeposit.addEventListener('click',depositChecking);
+savingsDeposit.addEventListener('click',depositSavings);
+checkingWithdraw.addEventListener('click',withdrawChecking);
+savingsWithdraw.addEventListener('click',withdrawSavings);
 
-
+function test(){
+  console.log(this.elementPreviousSibling.elementPreviousSibling);
+};
 
 function checkCheckingBalanceZero(){
   var chekcingAccount = document.querySelector('#checkingAccount');
@@ -35,12 +38,61 @@ function checkSavingsBalanceZero(){
   }
 };
 
-function deposit(number, account){
+function depositChecking(){
+  if(+checkingAmount.value >= 0){
+    transaction('checking', (+checkingAmount.value));
+    checkingAmount.value = '';
+  }
+};
+
+function depositSavings(){
+  if(+savingsAmount.value >= 0){
+    transaction('savings', (+savingsAmount.value));
+    savingsAmount.value = '';
+  }
+};
+
+function withdrawChecking(){
+  withdrawal('checking', checkingAmount.value);
+  checkingAmount.value= '';
+};
+
+function withdrawSavings(){
+  withdrawal('savings', savingsAmount.value);
+  savingsAmount.value = '';
+};
+
+function withdrawal(account, number){
+  var checkingBalanceNumber = +checkingBalance.innerHTML.slice(excludeDollarSign);
+  var savingsBalanceNumber = +savingsBalance.innerHTML.slice(excludeDollarSign);
+  if(number >= 0 && checkingBalanceNumber + savingsBalanceNumber >= number){
+    if(account === 'checking'){
+     if(checkingBalanceNumber >= number){
+        transaction('checking', -number);
+      } else {
+        transaction('savings',checkingBalanceNumber + -number);
+        transaction('checking', -checkingBalanceNumber);
+      }
+    } else{
+      if(savingsBalanceNumber >= number){
+        transaction('savings', (-number));
+      } else {
+        transaction('checking',savingsBalanceNumber + (-number));
+        transaction('savings', -savingsBalanceNumber);
+      }
+    }
+  }
+};
+
+
+function transaction(account, number){
   if (account === 'checking'){
-    var checkingBalanceNumber = +checkingBalance.innerHTML.slice(1);
+    var checkingBalanceNumber = +checkingBalance.innerHTML.slice(excludeDollarSign);
     checkingBalance.innerHTML = '$' + (checkingBalanceNumber + number).toString();
+    checkCheckingBalanceZero();
   }else{
-    var savingsBalanceNumber = +csavingsBalance.innerHTML.slice(1);
+    var savingsBalanceNumber = +savingsBalance.innerHTML.slice(excludeDollarSign);
     savingsBalance.innerHTML = '$' + (savingsBalanceNumber + number).toString();
+    checkSavingsBalanceZero();
   }
 };
