87665e48fa90b1154227017186b138e79bce675e
jayzz55 <jayzzwijono@yahoo.com>
2015-03-15 12:55:43 +1100

    refactor the code and add comments

--- Jaya Wijono/week1/atm/js/atm.js
+++ Jaya Wijono/week1/atm/js/atm.js
@@ -1,3 +1,4 @@
+// Setting the initial global variables
 var checkingBalance = document.querySelector("#balance1");
 var savingsBalance = document.querySelector("#balance2");
 var checkingAmount = document.querySelector("#checkingAmount");
@@ -8,36 +9,37 @@ var savingsDeposit = document.querySelector("#savingsDeposit");
 var savingsWithdraw = document.querySelector("#savingsWithdraw");
 var excludeDollarSign = 1;
 
+// calling the function to check if the balance is zero to make account background color as red
 checkCheckingBalanceZero();
 checkSavingsBalanceZero();
 
+// buttons waiting for 'click' event
 checkingDeposit.addEventListener('click',depositChecking);
 savingsDeposit.addEventListener('click',depositSavings);
 checkingWithdraw.addEventListener('click',withdrawChecking);
 savingsWithdraw.addEventListener('click',withdrawSavings);
 
-function test(){
-  console.log(this.elementPreviousSibling.elementPreviousSibling);
-};
-
+// function to make checking background color red if balance is $0
 function checkCheckingBalanceZero(){
   var chekcingAccount = document.querySelector('#checkingAccount');
-  if (+checkingBalance.innerHTML.slice(1) === 0){
+  if (+checkingBalance.innerHTML.slice(excludeDollarSign) === 0){
     checkingAccount.className = checkingAccount.className + " zero";
   } else {
     checkingAccount.className = "account";
   }
 };
 
+// function to make savings background color red if balance is $0
 function checkSavingsBalanceZero(){
   var savingsAccount = document.querySelector('#savingsAccount');
-  if (+savingsBalance.innerHTML.slice(1) === 0){
+  if (+savingsBalance.innerHTML.slice(excludeDollarSign) === 0){
     savingsAccount.className = savingsAccount.className + " zero";
   } else {
     savingsAccount.className = "account";
   }
 };
 
+// function to deposit money into checking acount
 function depositChecking(){
   if(+checkingAmount.value >= 0){
     transaction('checking', (+checkingAmount.value));
@@ -45,6 +47,7 @@ function depositChecking(){
   }
 };
 
+// function to deposit money into savings acount
 function depositSavings(){
   if(+savingsAmount.value >= 0){
     transaction('savings', (+savingsAmount.value));
@@ -52,16 +55,19 @@ function depositSavings(){
   }
 };
 
+// function to withdraw money from checking acount
 function withdrawChecking(){
   withdrawal('checking', checkingAmount.value);
   checkingAmount.value= '';
 };
 
+// function to withdraw money from savings acount
 function withdrawSavings(){
   withdrawal('savings', savingsAmount.value);
   savingsAmount.value = '';
 };
 
+// function for money withdrawal with overdraft protection
 function withdrawal(account, number){
   var checkingBalanceNumber = +checkingBalance.innerHTML.slice(excludeDollarSign);
   var savingsBalanceNumber = +savingsBalance.innerHTML.slice(excludeDollarSign);
@@ -84,7 +90,7 @@ function withdrawal(account, number){
   }
 };
 
-
+// function where both withdrawal and deposit function call to add or minus the account balance
 function transaction(account, number){
   if (account === 'checking'){
     var checkingBalanceNumber = +checkingBalance.innerHTML.slice(excludeDollarSign);
