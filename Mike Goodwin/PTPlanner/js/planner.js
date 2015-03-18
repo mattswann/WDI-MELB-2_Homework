858e3b92b3234b4fe6111b7d9f9223be6dac4beb
Mike Goodwin <goodo73@gmail.com>
2015-03-18 17:24:32 +1100

    Ecard lab

--- Mike Goodwin/PTPlanner/js/planner.js
+++ Mike Goodwin/PTPlanner/js/planner.js
@@ -7,30 +7,54 @@ var metro = {
 // Parliament removed from the Glen Waverly line to create the Pakenham line
 
 var lines = Object.keys(metro);
-// var userLineStart = "Alamein";
-// var userStopStart = "Glenferrie";
-// var userLineEnd = "Alamein";
-// var userStopEnd = "Hawthorn";
 
-var userLineStart = prompt("On which line do you wish to start your journey?");
-while (!validEntry(lines,userLineStart)) {
-	var userLineStart = prompt("That line is not valid. Please re-enter.");
+loadLines(lines);
+
+function loadLines(arr) {
+	var startLine = document.getElementById("startLine");
+	var startStop = document.getElementById("startStop");
+	var endLine = document.getElementById("endLine");
+	var endStop = document.getElementById("endStop");
+	// debugger;
+	for (var i = 0; i < arr.length; i++) {
+		var option = document.createElement("option");
+		option.text = arr[i];
+		startLine.add(option);
+		// loadStops(startStop,metro[arr[i]]);
+		var option = document.createElement("option");
+		option.text = arr[i];
+		endLine.add(option);
+		// loadStops(endStop,metro[arr[i]]);
+	}
 }
 
-var userStopStart = prompt("At which stop do you wish to start your journey?");
-while (!validEntry(metro[userLineStart],userStopStart)) {
-	var userStopStart = prompt("That line is not valid. Please re-enter.");
+function loadStops(listElement,stopsArray) {
+	for (var i = 0; i < stopsArray.length; i++) {
+		var option = document.createElement("option");
+		option.text = stopsArray[i];
+		listElement.add(option);
+	};
 }
 
-var userLineEnd = prompt("On which line do you wish to end your journey?");
-while (!validEntry(lines,userLineEnd)) {
-	var userLineEnd = prompt("That line is not valid. Please re-enter.");
-}
+// var userLineStart = prompt("On which line do you wish to start your journey?");
+// while (!validEntry(lines,userLineStart)) {
+// 	var userLineStart = prompt("That line is not valid. Please re-enter.");
+// }
 
-var userStopEnd = prompt("At which stop do you wish to end your journey?");
-while (!validEntry(metro[userLineEnd],userStopEnd)) {
-	var userStopEnd = prompt("That line is not valid. Please re-enter.");
-}
+// var userStopStart = prompt("At which stop do you wish to start your journey?");
+// while (!validEntry(metro[userLineStart],userStopStart)) {
+// 	var userStopStart = prompt("That line is not valid. Please re-enter.");
+// }
+
+// var userLineEnd = prompt("On which line do you wish to end your journey?");
+// while (!validEntry(lines,userLineEnd)) {
+// 	var userLineEnd = prompt("That line is not valid. Please re-enter.");
+// }
+
+// var userStopEnd = prompt("At which stop do you wish to end your journey?");
+// while (!validEntry(metro[userLineEnd],userStopEnd)) {
+// 	var userStopEnd = prompt("That line is not valid. Please re-enter.");
+// }
 
 var interchange = "Richmond";
 var totalStops = 0;
@@ -39,7 +63,7 @@ var msg = "You're journeying from " + userStopStart;
 msg += ", on the " + userLineStart + " line, to ";
 msg += userStopEnd + ", on the " + userLineEnd + " line.";
 
-console.log(msg);
+// console.log(msg);
 
 if (userLineStart === userLineEnd) {
 // Start and end stations are on the same line
@@ -114,3 +138,4 @@ function findIndex (line,stop) {
 // Determines the index value of a stop on a line
 	return line.indexOf(stop);
 }
+
