fb68883d36608fcdd194c842d3e205bbf5345c80
Mike Goodwin <goodo73@gmail.com>
2015-03-22 22:07:01 +1100

    Modified to output to web page

--- Mike Goodwin/PTPlanner/js/planner.js
+++ Mike Goodwin/PTPlanner/js/planner.js
@@ -4,113 +4,128 @@ var metro = {
 	Sandringham: ["Southern Cross", "Richmond", "South Yarra", "Prahran", "Windsor"],
 	Pakenham: ["Parliament","Richmond","Hawksburn","Toorak","Armadale","Malvern","Caulfield"]
 }
-// Parliament removed from the Glen Waverly line to create the Pakenham line
 
-var lines = Object.keys(metro);
-
-loadLines(lines);
-
-function loadLines(arr) {
-	var startLine = document.getElementById("startLine");
-	var startStop = document.getElementById("startStop");
-	var endLine = document.getElementById("endLine");
-	var endStop = document.getElementById("endStop");
-	// debugger;
-	for (var i = 0; i < arr.length; i++) {
-		var option = document.createElement("option");
-		option.text = arr[i];
-		startLine.add(option);
-		// loadStops(startStop,metro[arr[i]]);
-		var option = document.createElement("option");
-		option.text = arr[i];
-		endLine.add(option);
-		// loadStops(endStop,metro[arr[i]]);
-	}
-}
-
-function loadStops(listElement,stopsArray) {
-	for (var i = 0; i < stopsArray.length; i++) {
-		var option = document.createElement("option");
-		option.text = stopsArray[i];
-		listElement.add(option);
-	};
-}
-
-// var userLineStart = prompt("On which line do you wish to start your journey?");
-// while (!validEntry(lines,userLineStart)) {
-// 	var userLineStart = prompt("That line is not valid. Please re-enter.");
-// }
-
-// var userStopStart = prompt("At which stop do you wish to start your journey?");
-// while (!validEntry(metro[userLineStart],userStopStart)) {
-// 	var userStopStart = prompt("That line is not valid. Please re-enter.");
-// }
-
-// var userLineEnd = prompt("On which line do you wish to end your journey?");
-// while (!validEntry(lines,userLineEnd)) {
-// 	var userLineEnd = prompt("That line is not valid. Please re-enter.");
-// }
-
-// var userStopEnd = prompt("At which stop do you wish to end your journey?");
-// while (!validEntry(metro[userLineEnd],userStopEnd)) {
-// 	var userStopEnd = prompt("That line is not valid. Please re-enter.");
-// }
-
+var lines;
+var userLineStart;
+var userLineEnd;
+var userStopStart;
+var userStopEnd;
 var interchange = "Richmond";
-var totalStops = 0;
-var journeyLeg;
-var msg = "You're journeying from " + userStopStart;
-msg += ", on the " + userLineStart + " line, to ";
-msg += userStopEnd + ", on the " + userLineEnd + " line.";
 
-// console.log(msg);
+init();
 
-if (userLineStart === userLineEnd) {
-// Start and end stations are on the same line
+/*------------------------------------------*/
+/*      E V E N T   L I S T E N E R S       */
+/*------------------------------------------*/
 
-	// Calculate the trip between the start station and the end station on the line
-	journeyLeg = getStops(metro[userLineStart],userStopStart,userStopEnd)
-	totalStops += journeyLeg.length - 1;
+document.getElementById("startLine").addEventListener("change",function() {
+	removeChildElements(document.getElementById("startStop"));
+	userLineStart = getSelectedValue(document.getElementById("startLine"));
+	loadSelectionElement(document.getElementById("startStop"),metro[userLineStart]);
+});
 
-	displayStops(journeyLeg);
-} else {
-// Start and end stations are on different lines
+document.getElementById("endLine").addEventListener("change",function() {
+	removeChildElements(document.getElementById("endStop"));
+	userLineEnd = getSelectedValue(document.getElementById("endLine"));
+	loadSelectionElement(document.getElementById("endStop"),metro[userLineEnd]);
+});
 
-	// Calculate the trip between the start station and the interchange station on the first line
-	journeyLeg = getStops(metro[userLineStart],userStopStart,interchange)
-	totalStops += journeyLeg.length - 1;
+document.getElementById("submit").addEventListener("click",function() {
+	userStopStart = getSelectedValue(document.getElementById("startStop"));
+	userStopEnd = getSelectedValue(document.getElementById("endStop"));
 
-	displayStops(journeyLeg);
+	var trip = buildJourney();
+	var totalStops = trip.length -  1;
 
-	// Calculate the trip between the interchange station and the end station on the 2nd line
-	journeyLeg = getStops(metro[userLineEnd],interchange,userStopEnd)
-	totalStops += journeyLeg.length - 1;
-	
-	// Prevent the interchange station from being displayed twice
-	journeyLeg.shift();
+	var msg = "You're journey from " + userStopStart;
+	msg += ", on the " + userLineStart + " line, to ";
+	msg += userStopEnd + ", on the " + userLineEnd + " line, ";
+	msg += "will take you through " + totalStops + " stops.";
 
-	displayStops(journeyLeg);
-}
+	document.getElementById("description").innerHTML = msg;
 
-console.log("Your journey will take you through " + totalStops + " stops.");
+	displayStops(trip);
+});
+
+
 
 /*------------------------------------------*/
 /*          F U N C T I O N S               */
 /*------------------------------------------*/
 
-function validEntry (array,value) {
-// Tests if a specified item exists with the provided array
-	if (array.indexOf(value) === -1) {
-		return false;
-	} else {
-		return true;
+function init() {
+	lines = Object.keys(metro);
+
+	loadSelectionElement(document.getElementById("startLine"),lines);
+	loadSelectionElement(document.getElementById("endLine"),lines);
+
+	userLineStart = getSelectedValue(document.getElementById("startLine"));
+	userLineEnd = getSelectedValue(document.getElementById("endLine"));
+
+	loadSelectionElement(document.getElementById("startStop"),metro[userLineStart]);
+	loadSelectionElement(document.getElementById("endStop"),metro[userLineEnd]);
+}
+
+function getSelectedValue (selectionElement) {
+// Returns the currently selected value of the selection element
+	return selectionElement.value;
+}
+
+function loadSelectionElement(selectionElement,array) {
+	// Populate the selection element using the array
+	for (var i = 0; i < array.length; i++) {
+		var option = document.createElement("option");
+		option.text = array[i];
+		selectionElement.add(option);
 	}
+}
+
+function removeChildElements(parentElement) {
+	while (parentElement.lastChild) {
+		parentElement.removeChild(parentElement.lastChild);
+	}
+}
+
+function buildJourney() {
+	if (userLineStart === userLineEnd) {
+	// Start and end stations are on the same line
+
+		// Calculate the trip between the start station and the end station on the line
+		var journey = getStops(metro[userLineStart],userStopStart,userStopEnd)
+
+	} else {
+	// Start and end stations are on different lines
+
+		// Retrieve the stops on the trip between the start station and the interchange station on the first line
+		var journeyLeg = getStops(metro[userLineStart],userStopStart,interchange)
+
+		var journey = journeyLeg;
+
+		// Retrieve the stops on the trip between the interchange station and the end station on the 2nd line
+		journeyLeg = getStops(metro[userLineEnd],interchange,userStopEnd)
+		
+		// Prevent the interchange station from being displayed twice
+		journeyLeg.shift();
+
+		journey = journey.concat(journeyLeg);
+	}
+	
+	return journey;
 }
 
 function displayStops (journey) {
 // Display the stops on the journey array
+	removeChildElements(document.getElementById("stops"));
+
 	for (var i = 0; i < journey.length; i++) {
-		console.log(journey[i]);
+		var listItem = document.createElement("li");
+		listItem.innerHTML = journey[i];
+		
+		if (i < journey.length - 1) {
+			listItem.innerHTML += " >> ";
+		}
+
+		document.getElementById("stops").appendChild(listItem);
 	}
 }
 
