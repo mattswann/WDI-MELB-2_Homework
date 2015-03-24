4870183b898d9ca823cbfa55549328b8bdf80128
Matt Cameron <mcameron@avalanche.com.au>
2015-03-24 17:57:59 +1100

    intro to localStorage

--- Matt Cameron/classwork/jquery/todor/todor.js
+++ Matt Cameron/classwork/jquery/todor/todor.js
@@ -1,4 +1,8 @@
 function newTask(text) {
+		var tasks = JSON.parse(localStorage.getItem('tasks'));
+		tasks.push(text);
+		localStorage.setItem('tasks', JSON.stringify(tasks));
+
 		var $tasks = $('#tasks');
 		var $newTask = $('<li>').html(text);
 		$tasks.append($newTask);
@@ -7,6 +11,25 @@ function newTask(text) {
 
 $(document).ready(function() {
 
+	if (!localStorage.getItem('tasks')) { localStorage.setItem('tasks', JSON.stringify([]) ); };
+	if (!localStorage.getItem('doneTasks')) { localStorage.setItem('doneTasks', JSON.stringify([]) ); };
+
+
+	var tasks = JSON.parse(localStorage.getItem('tasks'));
+	var doneTasks = JSON.parse(localStorage.getItem('doneTasks'));
+
+	$.each(tasks, function(index, element) {
+		var $tasks = $('#tasks');
+		var $newTask = $('<li>').html(element);
+		$('#tasks').append($newTask);
+	});
+
+		$.each(doneTasks, function(index, element) {
+		var $doneTasks = $('#doneTasks');
+		var $newTask = $('<li>').html(element);
+		$('#completed').append($newTask);
+
+	});
 
 	$('form').on('submit', function(event) {
 		event.preventDefault();
@@ -19,8 +42,23 @@ $(document).ready(function() {
 
 	// event delegation
 	$('#tasks').on('click', 'li', function() {
-		$(this).addClass('done');
+
+		// move to completed ul
 		$('#completed').append(this);
+
+
+		var currentTask = this.innerHTML;
+
+		// add to doneTasks in localStorage
+		var completedTasks = JSON.parse(localStorage.getItem('doneTasks'));
+		completedTasks.push(currentTask);
+		localStorage.setItem('doneTasks', JSON.stringify(completedTasks));
+
+		// remove from tasks in localStorage
+		var toDos = JSON.parse(localStorage.getItem('tasks'));
+		var idx = toDos.indexOf(currentTask);
+		toDos.splice(idx, 1);
+		localStorage.setItem('tasks', JSON.stringify(toDos));
 	})
 
 });
