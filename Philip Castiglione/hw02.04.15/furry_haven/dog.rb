3c810c0a18955a62cfa726711ef9a5f17704fce6
Philip Castiglione <philipcastiglione@gmail.com>
2015-04-06 15:51:00 +1000

    added furries to homework

---
+++ Philip Castiglione/hw02.04.15/furry_haven/dog.rb
@@ -0,0 +1,109 @@
+def dog
+  sleep 1
+  puts "                                         do. "
+  sleep 0.05
+  puts "                                        :NOX "
+  sleep 0.05
+  puts "                                       ,NOM@: "
+  sleep 0.05
+  puts "                                       :NNNN: "
+  sleep 0.05
+  puts "                                       :XXXON "
+  sleep 0.05
+  puts "                                       :XoXXX. "
+  sleep 0.05
+  puts "                                       MM;ONO: "
+  sleep 0.05
+  puts "  .oob..                              :MMO;MOM "
+  sleep 0.05
+  puts " dXOXYYNNb.                          ,NNMX:MXN "
+  sleep 0.05
+  puts " Mo''  '':Nbb                        dNMMN MNN: "
+  sleep 0.05
+  puts " Mo  'O;; ':Mb.                     ,MXMNM MNX: "
+  sleep 0.05
+  puts " @O :;XXMN..'X@b.                  ,NXOMXM MNX: "
+  sleep 0.05
+  puts " YX;;NMMMM@M;;OM@o.                dXOOMMN:MNX: "
+  sleep 0.05
+  puts " 'MOONM@@@MMN:':NONb.            ,dXONM@@MbMXX: "
+  sleep 0.05
+  puts "  MOON@M@@MMMM;;:OOONb          ,MX'^':ONMMMMX: "
+  sleep 0.05
+  puts "  :NOOM@@MNNN@@X;^^XNN@Mb     .dP^'   ,..OXM@N: "
+  sleep 0.05
+  puts "   MOON@@MMNXXMMO  :M@@M...@o.oN^^^:OOOXNNXXOo: "
+  sleep 0.05
+  puts "   :NOX@@@MNXXXMNo :MMMM@K^`,:;NNM@@NXM@MNO;.'N. "
+  sleep 0.05
+  puts "    NO:X@@MNXXX@@O:'X@@@@MOOOXMM@M@NXXN@M@NOO ''b "
+  sleep 0.05
+  puts "    `MO.'NMNXXN@@N: 'XXM@NMMXXMM@M@XO^'^XM@X;.  :b "
+  sleep 0.05
+  puts "     YNO;'^NXXXX@M;;::^XMNN:^^ON@@MO: ,;;.:Y@X: :OX. "
+  sleep 0.05
+  puts "      Y@Mb;;XNMM@@@NO: ':O: 'OXN@@MO^ ONMMX:`XO; :X@. "
+  sleep 0.05
+  puts "      '@XMX':OX@@MN:    ;O;  :OX@MO^ 'OMM@N; ':OO;N@N "
+  sleep 0.05
+  puts "       YN;^:.:OXMX^': ,:NNO;';XMMX:  ,;@@MNN.'.:O;:@X: "
+  sleep 0.05
+  puts "       `@N;;XOOOXO;;:O;:@MOO;:O:^^ ,oMP@@K^YM.;NMO;`NM "
+  sleep 0.05
+  puts "        `@@MN@MOX@@MNMN;@@MNXXOO: ,d@NbMMP'd@@OX@NO;.'bb. "
+  sleep 0.05
+  puts "       .odMX@@XOOM@M@@XO@MMMMMMNNbN^YNNNXoNMNMO^OXXNO..^^;o. "
+  sleep 0.05
+  puts "     .ddMNOO@@XOOM@@XOONMMM@@MNXXMMo;.^' .^:OXO ':.'^'^'  '^^o. "
+  sleep 0.05
+  puts "    'N@@X;,M@MXOOM@OOON@MM@MXOO:^:ONMNXXOXX:OOO               ^^ob. "
+  sleep 0.05
+  puts "   ')@MP^';@@XXOOMMOOM@MNNMOO^^   '^OXM@MM: :OO.        :...';o;.;Xb. "
+  sleep 0.05
+  puts "  .@@MX^ ;X@@XXOOM@OOXXOO:o:'      :OXMNO^' ;OOO;.:     ,OXMOOXXXOOXMb "
+  sleep 0.05
+  puts " ,dMOo:  oO@@MNOON@N:::^      .    ,;O:^^'  .dMXXO:    ,;OX@XO^^:ON@M@ "
+  sleep 0.05
+  puts ":Y@MX:.  oO@M@NOXN@NO. ..: ,;;O;.       :.OX@@MOO;..   .OOMNMO.;XN@M@P "
+  sleep 0.05
+  puts ",MP^OO'  oO@M@O:ON@MO;;XO;:OXMNOO;.  ,.;.;OXXN@MNXO;.. oOX@NMMN@@@@@M: "
+  sleep 0.05
+  puts "`' ^O:;;OON@@MN::XNMOOMXOOOM@@MMNXO:;XXNNMNXXXN@MNXOOOOOXNM@NM@@@M@MP "
+  sleep 0.05
+  puts "   :XN@MMM@M@M:  :'OON@@XXNM@M@MXOOdN@@@MM@@@@MMNNXOOOXXNNN@@M@MMMM^' "
+  sleep 0.05
+  puts "   .oNM@MM@ONO'   :;ON@@MM@MMNNXXXM@@@@M@PY@@MMNNNNNNNNNNNM@M@M@@P' "
+  sleep 0.05
+  puts "  ;O:OXM@MNOOO.   'OXOONM@MNNMMXON@MM@@b. 'Y@@@@@@@@@@@@@M@@MP^'^ "
+  sleep 0.05
+  puts " ;O':OOXNXOOXX:   :;NMO:^:NMMMXOOX@MN@@@@b.:M@@@M@@@MMM@^^^^ "
+  sleep 0.05
+  puts " :: ;^OOOOOO@N;:  'ON@MO.'^:^^OOOO@@NNMN@@@. Y@@@MMM@@@@b "
+  sleep 0.05
+  puts " :;   ':O:oX@@O;;  ;O@@XO'   ^oOOOOXMMNMNNN@MN^^YMNMMM@@MMo. "
+  sleep 0.05
+  puts " :N:.   ''oOM@NMo.::OX@NOOo.  ;OOOXXNNNMMMNXNM@bd@MNNMMM@MM@bb "
+  sleep 0.05
+  puts "  @;O .  ,OOO@@@MX;;ON@NOOO.. ' ':OXN@NNN@@@@@M@@@@MNXNMM@MMM@, "
+  sleep 0.05
+  puts "  M@O;;  :O:OX@@M@NXXOM@NOO:;;:,;;ON@NNNMM'`^@@M@@@@@MXNMMMMM@N "
+  sleep 0.05
+  puts "  N@NOO;:oO;O:NMMM@M@OO@NOO;O;oOOXN@NNM@@'   `Y@NM@@@@MMNNMM@MM "
+  sleep 0.05
+  puts "  ::@MOO;oO:::OXNM@@MXOM@OOOOOOXNMMNNNMNP      ^^MNNM@@@MMMM@MP "
+  sleep 0.05
+  puts "    @@@XOOO':::OOXXMNOO@@OOOOXNN@NNNNNNNN        '`YMM@@@MMM@P' "
+  sleep 0.05
+  puts "    MM@@M:'''' O:^:ONOO@MNOOOOXM@NM@NNN@P           ^`^^^MM' "
+  sleep 0.05
+  puts "    ''MM@:     ^' 'OOONMOYOOOOO@MM@MNNM^ "
+  sleep 0.05
+  puts "      YM@'         :OOMN: :OOOO@MMNOXM' "
+  sleep 0.05
+  puts "      `:P           :oP''  ^'OOM@NXNM' "
+  sleep 0.05
+  puts "       `'                    ':OXNP' "
+  sleep 0.05
+  puts "                               '^' "
+  sleep 1
+end
