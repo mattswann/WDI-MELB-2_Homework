873fe5803187b7b025935a458d319026f0120745
Matt Cameron <mcameron@avalanche.com.au>
2015-04-08 16:29:16 +1000

    MMDB pulling from database or OMDB

--- Matt Cameron/homework/5/app.rb
+++ Matt Cameron/homework/5/app.rb
@@ -4,6 +4,8 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'httparty'
 require 'pry'
+require 'pg'
+require 'uri'
 
 
 get '/' do
@@ -16,11 +18,63 @@ get '/about' do
 end
 
 get '/movie' do
-	@movie = HTTParty.get("http://www.omdbapi.com/?t=#{params['title']}&plot=full")
+	# normalise the input
+	params['title'] = params['title'].capitalize
 
-	if @movie['Response'] == "True"
-	 erb :title
+	# Get the entered movie from OMDB
+	url = "http://www.omdbapi.com/?t=#{params['title']}&plot=full"
+	@movie = HTTParty.get(URI.escape(url))
+
+	# if movie exists in OMDB
+	if @movie['Title'] == params['title']
+
+		exists_already = run_sql("SELECT * FROM movies WHERE title = '#{params['title']}'")
+
+		# if the movie is not already in our database, add it
+		if exists_already.count == 0
+			raw_result = HTTParty.get(URI.escape(url))
+			@movie = {}
+			raw_result.each do |key, value|
+				@result[key.downcase] = value
+			end
+
+			sql = "INSERT INTO movies (title, poster, rated, runtime, year, languages, imdbrating, director, plot, actors) VALUES (
+			'#{ escape_apostrophes @movie['Title'] }',
+			 '#{ @movie['Poster'] }',
+			 '#{ @movie['Rated'] }',
+			 '#{ @movie['Runtime'] }',
+			 '#{ @movie['Year'] }',
+			 '#{ @movie['Language'] }',
+			 '#{ @movie['imdbRating'] }',
+			 '#{ escape_apostrophes @movie['Director'] }',
+			 '#{ escape_apostrophes @movie['Plot'] }',
+			 '#{ escape_apostrophes @movie['Actors']}'
+			 )"
+			run_sql(sql)
+
+			#if the movie is already in our database, set @movie from our database
+	 	else
+	 		sql = "SELECT * FROM movies WHERE title = '#{ params['title'] }' "
+	 		 @rows = run_sql(sql)
+	 		 @movie = @rows[0]
+	 	end
+	 	erb :title
+
+	 	# if the movie doesn't exist on OMDB, do a fuzzy search and display the results
 	else
-		erb :error
+		@results = HTTParty.get("http://www.omdbapi.com/?s=#{params['title'] }&plot=full")['Search']
+		erb :results
 	end
+end
+
+
+def run_sql(sql)
+	db = PG.connect(:dbname => 'mmdb')
+  @rows = db.exec(sql)
+  db.close
+  return @rows
+end
+
+def escape_apostrophes(string)
+	string.gsub("'", "''")
 end
