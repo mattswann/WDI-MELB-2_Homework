38d32d9b8ef1295dccada08fc5fe650e942db99d
Matt Cameron <mcameron@avalanche.com.au>
2015-05-25 13:03:02 +1000

    Practice with ActiveRecord

--- Matt Cameron/classwork/7-tdd/tdd/goodfoodhunting/app/models/dish.rb
+++ Matt Cameron/classwork/7-tdd/tdd/goodfoodhunting/app/models/dish.rb
@@ -1,6 +1,17 @@
 class Dish < ActiveRecord::Base
 	has_many :taggings
 	has_many :tags, :through => :taggings
+	has_many :likes
 
 	mount_uploader :photo, PhotoUploader
+
+	def self.top_three
+		# something like this...
+		dishes = Dish.joins(:likes).group(:title, :dish_id).order(:title).limit(3)
+		# not working just yet
+	end
+
+	def like_count
+		likes.count
+	end
 end
